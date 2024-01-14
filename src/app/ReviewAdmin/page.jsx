8b0e6ada0eb225984b04e32c13a0df97c3d0c105
line16 <linesorensen53@gmail.com>
2024-01-14 15:20:41 +0100

    update

--- src/app/ReviewAdmin/page.jsx
+++ src/app/ReviewAdmin/page.jsx
@@ -4,6 +4,10 @@ import useRequestData from "../../../Hooks/useRequestData";
 import { useEffect } from "react";
 import Navbar from "../layout/Navbar";
 import Link from "next/link";
+// icons
+import { TbUserEdit } from "react-icons/tb"; //Edit
+import { RiDeleteBin4Fill } from "react-icons/ri"; //delete
+import { RiUserAddFill } from "react-icons/ri"; //Add
 
 const ReviewAdmin = () => {
   const { data, isLoading, error, makeRequest } = useRequestData();
@@ -20,11 +24,14 @@ const ReviewAdmin = () => {
     makeRequest("http://localhost:5023/reviews");
   }, []);
 
-  const handleDelete = (reviewID, reviewAuthor) =>{
+  const handleDelete = (reviewID, reviewAuthor) => {
     if (window.confirm("du sletter " + reviewAuthor)) {
-        makeRequestDelete("http://localhost:5023/reviews/admin/" + reviewID, "DELETE");
+      makeRequestDelete(
+        "http://localhost:5023/reviews/admin/" + reviewID,
+        "DELETE"
+      );
     }
-  }
+  };
 
   return (
     <div>
@@ -32,49 +39,62 @@ const ReviewAdmin = () => {
       <h1 className="font-bold text-3xl text-center m-4">Admin for Reviews</h1>
       {/* ---- */}
       <div className="overflow-x-auto">
-    
-
-      <table className="table text-black m-auto bg-green-100 rounded-md shadow-md w-10/12 h-auto">
-        <thead>
-          <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th className="text-black">
-              <Link href="/ReviewCreate">
-              </Link>
-            </th>
-          </tr>
-          <tr>
-            <th>Id</th>
-            <th>Navn</th>
-            <th>Review</th>
-            <th>Ret</th>
-            <th>Slet</th>
-          </tr>
-        </thead>
-        <tbody className="m-auto rounded-md p-3">
-          {data &&
-             data.map((p) => (
-              <tr className="border-green-200 border-2 py-3 px-6 rounded-md m-3" key={p._id}>
-                <td className="py-3 px-6">{p._id}</td>
-                <td className="py-3 px-6">{p.author}</td>
-                <td className="py-3 px-6">{p.content}</td>
-                <td className="py-3 px-6">
-                  edit
-                </td>
-                <td className="py-3 px-6">
-                  <button className="btn btn-neutral" onClick={() => handleDelete(p._id, p.author)}>
-                    delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+        <div className="w-52 bg-gray-200 my-5 mx-auto h-10 text-center p-2 rounded-md shadow-md">
+          <p>
+            lav et nyt review{" "}
+            <Link className="text-2xl float-right" href="/ReviewCreate">
+              <RiUserAddFill />
+            </Link>
+          </p>
+        </div>
+        <table className="table text-black m-auto bg-green-100 rounded-md shadow-md w-10/12 h-auto">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+            <tr>
+              <th>Id</th>
+              <th>Navn</th>
+              <th>Review</th>
+              <th>Ret</th>
+              <th>Slet</th>
+            </tr>
+          </thead>
+          <tbody className="m-auto rounded-md p-3">
+            {data &&
+              data.map((p) => (
+                <tr
+                  className="border-green-200 border-2 py-3 px-6 rounded-md m-3"
+                  key={p._id}
+                >
+                  <td className="py-3 px-6">{p._id}</td>
+                  <td className="py-3 px-6">{p.author}</td>
+                  <td className="py-3 px-6">{p.content}</td>
+                  <td className="py-3 px-6">
+                    <Link
+                      className="text-3xl"
+                      href={{ pathname: "/ReviewEdit", query: { id: p._id } }}
+                    >
+                      <TbUserEdit />
+                    </Link>
+                  </td>
+                  <td className="py-3 px-6">
+                    <button
+                      className="text-3xl"
+                      onClick={() => handleDelete(p._id, p.author)}
+                    >
+                      <RiDeleteBin4Fill />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-
-     </div>
   );
 };
 
--- src/app/ReviewEdit/page.jsx
+++ src/app/ReviewEdit/page.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+"use client";
 
-const reviewEdit = () => {
+import React, { useState, useEffect } from "react";
+import useRequestData from "../../../Hooks/useRequestData";
+import Navbar from "../layout/Navbar";
+import { useParams } from "react-router-dom";
+// icons
+import { TbUserEdit } from "react-icons/tb";
+
+const ReviewEdit = (params) => {
+
+  const ReviewID = params.searchParams.id
+
+  console.log(params)
+
+  const { data, isLoading, error, makeRequest } = useRequestData();
+
+  const {
+    data: dataPUT,
+    isLoading: isLoadingPUT,
+    error: errorput,
+    makeRequest: makeRequestPUT,
+  } = useRequestData();
+
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    makeRequest("http://localhost:5023/reviews/" + ReviewID);
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setAuthor(data.author);
+      setContent(data.content);
+    }
+  }, [data]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newReview = { author: author, content: content};
+
+    makeRequestPUT(
+      "http://localhost:5023/reviews/admin/" + ReviewID,
+      "PUT",
+      newReview
+    );
+  };
+
   return (
-    <div>reviewEdit</div>
-  )
-}
+    <div>
+      <Navbar />
+      {data && (
+        
+        <h1 className="text-center m-2 text-2xl font-bold">Vil du rette {data.author} review?</h1>
+      )}
+      <div>
+        {data && (
+          <form
+            onSubmit={handleSubmit}
+            className="m-auto flex flex-col justify-center w-4/6 bg-green-50 rounded-lg shadow-lg"
+          >
+            <label
+              htmlFor="vibTitle"
+              className="text-lg m-auto font-medium text-gray-800"
+            >
+              Navn
+            </label>
+            <input
+              className="m-auto w-80 h-8 rounded-md p-2 bg-gray-200 shadow-md"
+              type="text"
+              required
+              id="vibTitle"
+              onInput={(e) => setAuthor(e.target.value)}
+              value={author}
+            />
 
-export default reviewEdit
+            <label htmlFor="" className="text-lg m-2 font-medium text-gray-800 text-center">
+              indhold
+            </label>
+            <textarea
+              type="text"
+              required
+              className="text-black m-auto w-9/12 h-80 rounded-md p-2 bg-gray-200 shadow-md"
+              onInput={(e) => setContent(e.target.value)}
+              value={content}
+            />
+            
+            <p className="text-center bg-gray-200 w-6/12 mx-auto my-4 rounded-md shadow-md">personligt Id : {data._id}</p>
+
+            <button
+              type="submit"
+              className="w-44 h-8 bg-green-500 text-white rounded-md m-auto mt-10 mb-5"
+            >
+              Done
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ReviewEdit;
